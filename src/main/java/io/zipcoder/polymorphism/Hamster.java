e1b1b3de3733e306fd4be5376777241fe53f6617
Dikshasharmaa <diksha66sharma78@gmail.com>
2024-03-26 23:09:34 -0400

    Almost there

---
+++ src/main/java/io/zipcoder/polymorphism/Hamster.java
@@ -0,0 +1,16 @@
+package io.zipcoder.polymorphism;
+
+public class Hamster extends Pet {
+    public Hamster(String name){
+        super(name);
+    }
+    public Hamster(){
+        super();
+    }
+
+
+    @Override
+    public String speak() {
+        return "cho cho";
+    }
+}
--- src/main/java/io/zipcoder/polymorphism/MainApplication.java
+++ src/main/java/io/zipcoder/polymorphism/MainApplication.java
@@ -1,12 +1,50 @@
 package io.zipcoder.polymorphism;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MainApplication {
-    Scanner sc = new Scanner(System.in);
-    Integer numberOfPets = sc.nextInt();
-    String typesOfPets = sc.nextLine();
-    String nameOfPets = sc.nextLine();
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of pets");
+        int numberOfPets = sc.nextInt();
+
+        //create list
+        Pet[]pets = new Pet[numberOfPets];
+
+        //loop through num pets
+        for(int i =0; i<numberOfPets;i++){
+            System.out.println("Please choose one of the following pets:\n[Cat,Dog,Hamster]");
+            String typesOfPets = sc.next();
+            System.out.println("Enter name of your Pets:");
+            String nameOfPets = sc.next();
+            if(typesOfPets.equals("Cat")){
+                Cat cat = new Cat(nameOfPets);
+                pets[i] = cat;
+            } else if (typesOfPets.equals("Dog")) {
+                Dog dog = new Dog(nameOfPets);
+                pets[i] = dog;
+            } else if (typesOfPets == "Hamster") {
+                Hamster ham = new Hamster();
+                pets[i] = ham;
+
+            }else {
+                System.out.println("Incorrect pet type");
+                i--;
+            }
+
+        }
+        System.out.println("Here is the list of your pets!");
+        for(Pet p : pets){
+            System.out.println(p.getName()+" : "+p.speak());
+        }
+
+    }
+
+
+    //iterate through list and print name and speak sound
 
 
 }
